b639045d0ca224cb7b99812dfb4a7b3cd6431664
manbensam <abdulrahmanpancasila@gmail.com>
2014-04-10 16:11:45 +0700

    Pemrograman01RegulerGenap2013-2014

---
+++ boidata/src/Biodata.java
@@ -0,0 +1,15 @@
+//created by: Abdul Rahman
+//data		: 3 April 2014
+
+package Praktikum03a.bin;
+
+public class Biodata
+{
+	public static void main(String[] Abdulrahman)
+	{
+		System.out.println("Abdul Rahman");
+		System.out.println("Kalimantan Barat");
+		System.out.println("Belum Menikah");
+		System.out.println("085750074450");
+	}
+}
